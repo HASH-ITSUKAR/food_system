0ac53c18d9e3cd1d9ffb3b411cd409510c543540
ITSUKAR <hiraga.chevalier@gmail.com>
2025-07-15 02:09:16 -0500

    Corregido bd 4

--- database/migrations/2025_05_27_180429_create_detalle_pedido_table.php
+++ database/migrations/2025_05_27_180429_create_detalle_pedido_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
 
             $table->foreign('idPedido')
                 ->references('idPedido')
-                ->on('Pedido')
+                ->on('pedido')
                 ->onDelete('cascade');
             
             $table->foreign('idDishes')
